704c03c2ec5e20223837afde70a9d0c22a5147e1
กัมปนาท เก่งพรสกุล < >
2016-07-01 10:33:05 +0700

    add material input

--- public/containers/AddTodo.js
+++ public/containers/AddTodo.js
@@ -1,30 +1,67 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../redux/actions'
+import TextField from 'material-ui/TextField'
+import FlatButton from 'material-ui/FlatButton'
 
-let AddTodo = ({ dispatch }) => {
-  let input
+// let AddTodo = ({ dispatch }) => {
+//
+//   getInitialState(){
+//     return {
+//       input: ''
+//     }
+//   }
+//
+//   handleTextChange(e){
+//     this.setState({
+//       input: e.target.value
+//     });
+//   }
+//
+//   return (
+//     <div>
+//       <TextField value={this.state.input} onChange={this.handleTextChange} />
+//     </div>
+//   )
+// }
 
-  return (
-    <div>
-      <form onSubmit={e => {
-        e.preventDefault()
-        if (!input.value.trim()) {
-          return
-        }
-        dispatch(addTodo(input.value))
-        input.value = ''
-      }}>
-        <input ref={node => {
-          input = node
-        }} />
-        <button type="submit">
-          Add Todo
-        </button>
-      </form>
-    </div>
-  )
+class AddTodo extends Component {
+
+  constructor(props){
+    super(props);
+    this.handleChanged = this.handleChanged.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { input: '' };
+  }
+
+  handleChanged(event){
+    this.setState({
+      input: event.target.value
+    });
+  }
+
+  handleClick(){
+    if (!this.state.input.trim()) {
+      return;
+    }
+    let { dispatch } = this.props;
+    dispatch(addTodo(this.state.input));
+    this.setState({
+      input: ''
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        {this.state.input} <br/>
+      <TextField value={this.state.input} onChange={this.handleChanged} id="textInput" />
+        <FlatButton onClick={this.handleClick} label="Add todo" />
+      </div>
+    );
+  }
 }
+
 AddTodo = connect()(AddTodo)
 
 export default AddTodo
--- public/pages/TodoApp.js
+++ public/pages/TodoApp.js
@@ -2,12 +2,23 @@ import React from 'react'
 import Footer from '../components/Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
+import { Card, CardHeader, CardText } from 'material-ui/Card';
+
 
 const TodoApp = () => (
-  <div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
+  <div style={{margin: '20px'}}>
+    <Card>
+      <CardHeader
+        title="Todo List"
+        subtitle="Example todo list from redux example." />
+      <CardText>
+        <div>
+          <AddTodo />
+          <VisibleTodoList />
+          <Footer />
+        </div>
+      </CardText>
+    </Card>
   </div>
 )
 
